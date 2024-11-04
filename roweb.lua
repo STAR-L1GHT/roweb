7c5e4c5e9b1bbcc4ddef3e2a6c369b34e8ff0723
STARL1GHT <61551599+STAR-L1GHT@users.noreply.github.com>
2024-11-04 07:09:55 +0400

    Update roweb.lua

--- roweb.lua
+++ roweb.lua
@@ -1,6 +1,5 @@
 local RoWeb = {Version = "v1.4.5"}
 RoWeb.__index = RoWeb
-_G.ID = math.random(1000, 2000)
 
 local reqName = syn and "syn.request" or
 http_request and "http_request" or
@@ -9,38 +8,30 @@ HttpPost and "HttpPost"
 
 request = http_request or request or HttpPost or syn.request
 
-function RoWeb:new(url, options)
-    local options = options or {}
-    options.Url = url
-    options.Method = "GET"
-    
+function RoWeb:new(url)
+    local options = {
+        Url = url,
+        Method = "GET"
+    }
+
     local req = request(options)
     
-    local _page = {data = req, url = options.Url, options = options}
+    local _page = {data = req, url = options.Url}
     setmetatable(_page, self)
     return _page
 end
 
-function toJson(string)
-    local jsonData = nil
-    local s, err = pcall(function() 
-        local jsonTry = game:GetService("HttpService"):JSONDecode(string)  
-        jsonData = jsonTry
-    end)   
-    return jsonData or false
-end
-
-function RoWeb:toJson(string)
-    local jsonData = nil
-    local s, err = pcall(function() 
-        local jsonTry = game:GetService("HttpService"):JSONDecode(string)  
-        jsonData = jsonTry
-    end)   
-    return jsonData or false
-end
-
-function RoWeb:method()
-   return http_request or request or HttpPost or syn.request 
+function RoWeb:getBody(options)
+    if options and options.JSON then 
+        local success, jsonData = pcall(function() 
+            return game:GetService("HttpService"):JSONDecode(self.data.Body)  
+        end)
+        if not success then
+            return error("Cannot parse body: " .. tostring(jsonData))
+        end
+        return jsonData 
+    end
+    return self.data.Body
 end
 
 function RoWeb:getHeaders()
@@ -51,10 +42,17 @@ function RoWeb:getCookies()
     return self.data.Cookies
 end
 
+function RoWeb:method()
+    return reqName
+end
+
 function RoWeb:synspy(callback)
-    if (not syn) then return error("executor not supported") end
+    if not syn then return error("executor not supported") end
     local callbackType = type(callback)
-    if (callback == nil or callbackType:lower() ~= "function") then return error("callback function expected but got "..callbackType or nil) end
+    if callback == nil or callbackType:lower() ~= "function" then
+        return error("callback function expected but got " .. callbackType or nil)
+    end
+
     local instances = {
         HttpGet = RoWeb.Version,
         HttpGetAsync = RoWeb.Version,
@@ -62,31 +60,33 @@ function RoWeb:synspy(callback)
         HttpPostAsync = RoWeb.Version,
         GetObjects = RoWeb.Version
     }
+    
     local __nc
     local QID = _G.ID
-     __nc = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-         if (_G.ID ~= QID) then return __nc(self, ...) end
-          local method = getnamecallmethod()
-          if (instances[method]) then
-              callback({method = method, data = "game:"..method..'("'.. ... .. '")'})
-          end
+    __nc = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+        if _G.ID ~= QID then return __nc(self, ...) end
+        local method = getnamecallmethod()
+        if instances[method] then
+            callback({method = method, data = "game:" .. method .. '("' .. ... .. '")'})
+        end
         return __nc(self, ...)
-     end))
+    end))
+    
     local __rq
     __rq = hookfunction(request, newcclosure(function(req)
-        if (_G.ID ~= QID) then return __rq(req) end
+        if _G.ID ~= QID then return __rq(req) end
         coroutine.wrap(function()
             local s, res = pcall(__rq, req)
-            if (not s) then error("error played during getting response of hooking "..req.Url) end
+            if not s then error("Error occurred during getting response of hooking " .. req.Url) end
             callback({method = reqName, req = req, res = res})
         end)()
-        __rq(req)
+        return __rq(req)
     end))
-    if (syn) then
-
+    
+    if syn then
         local wb
         wb = hookfunction(syn.websocket.connect, function(...)
-            if (_G.ID ~= QID) then return wb(...) end
+            if _G.ID ~= QID then return wb(...) end
             callback({method = "syn.websocket.connect", Url = ...})
             return wb(...)
         end)
@@ -96,34 +96,30 @@ end
 function RoWeb:getFingerprint() 
     local fingerprint = {}
     local req = request({Url = "https://httpbin.org/headers"})
-   for _,v in pairs(toJson(req.Body).headers) do
-    if string.find(_:lower(), "fingerprint") then 
-    fingerprint[_] = v
-        else if string.find(_:lower(), "identifier") or string.find(_:lower(), "hwid")  then
+    for _, v in pairs(toJson(req.Body).headers) do
+        if string.find(_:lower(), "fingerprint") then 
             fingerprint[_] = v
-        else if string.find(_:lower(), "agent") then
+        elseif string.find(_:lower(), "identifier") or string.find(_:lower(), "hwid") then
             fingerprint[_] = v
-        else if string.find(_:lower(), "id") and _:lower() ~= "x-amzn-trace-id" then
+        elseif string.find(_:lower(), "agent") then
+            fingerprint[_] = v
+        elseif string.find(_:lower(), "id") and _:lower() ~= "x-amzn-trace-id" then
             fingerprint[_] = v
         end
     end
-    end
-    end
-   end
-   return fingerprint
+    return fingerprint
 end
 
-function RoWeb:getBody(options)
-    if (options) and  (options.JSON) and options.JSON == true then 
-        local jsonData = nil
-        local s, err = pcall(function() 
-        local jsonTry = game:GetService("HttpService"):JSONDecode(self.data.Body)  
-        jsonData = jsonTry
-        end)
-        if (err) then return error("Cant parse body") end
-        return jsonData 
-    end
-    return self.data.Body
+function toJson(string)
+    local jsonData = nil
+    local success, err = pcall(function() 
+        jsonData = game:GetService("HttpService"):JSONDecode(string)  
+    end)
+    return jsonData or false
+end
+
+function RoWeb:toJson(string)
+    return toJson(string)
 end
 
 return RoWeb
